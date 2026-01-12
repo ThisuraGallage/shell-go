4a636537275490622abfcddc741b96c503369cd7
ThisuraGallage <tggallage1@gmail.com>
2026-01-12 22:10:50 +0530

    Add user inputs

--- app/main.go
+++ app/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -9,4 +11,6 @@ var _ = fmt.Print
 
 func main() {
 	fmt.Print("$ ")
+	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
